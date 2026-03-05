92a890ce82a619f7731e36d814be0ccd0f02618b
Prateek Rastogi <prateek.rastogi@pm.me>
2026-03-05 18:11:10 +0530

    feat: Add phone call initiation to Try Demo button click handler.

--- src/app/components/header/TryDemoButton.tsx
+++ src/app/components/header/TryDemoButton.tsx
@@ -3,9 +3,13 @@
 import ScrollHandler from './ScrollHandler';
 
 const TryDemoButton = () => {
+  const handleTryDemoClick = () => {
+    window.location.href = 'tel:+14847897762';
+  };
+
   return (
     <>
-      <ScrollHandler onTryDemoClick={() => { }} />
+      <ScrollHandler onTryDemoClick={handleTryDemoClick} />
     </>
   );
 };
